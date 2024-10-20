b19e4432f1a0cf5b8c27453c35f268045d06906e
Julia Grigoreva <julia_panina@mail.ru>
2024-10-20 21:42:28 +0300

    fix: func setRange and guess

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -3,10 +3,12 @@ class GuessingGame {
 
     setRange(min, max) {
         this._arr = Array.apply(null, {length: max + 1}).map(Number.call, Number).slice(min);
+        return this._arr;
     }
 
     guess() {
-        this._result = this._arr[Math.floor((arr.length - 1) / 2)];
+        this._result = this._arr[Math.floor((this._arr.length - 1) / 2)];
+        return this._result;
     }
 
 
