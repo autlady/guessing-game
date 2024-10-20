ebd789c3bb0c5497a292e6392e060ac0a34e85f9
Julia Grigoreva <julia_panina@mail.ru>
2024-10-20 20:57:54 +0300

    feat: func lower and greater

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -5,16 +5,17 @@ class GuessingGame {
         this._arr = Array.apply(null, {length: max + 1}).map(Number.call, Number).slice(min);
     }
 
-    guess(arr) {
-        return arr[Math.floor((arr.length - 1) / 2)];
+    guess() {
+        this._result = this._arr[Math.floor((arr.length - 1) / 2)];
     }
 
-    lower() {
 
+    lower() {
+        this._arr.slice(nextIdx);
     }
 
     greater() {
-
+        this._arr.slice(0, this._result - 1);
     }
 }
 
