08b72ee64e19a5e902bb5f7ff1650f1dd543818d
Julia Grigoreva <julia_panina@mail.ru>
2024-10-20 22:31:11 +0300

    fix: func greter

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -7,17 +7,19 @@ class GuessingGame {
     }
 
     guess() {
+        this.index = Math.floor((this.arr.length - 1) / 2);
         this.result = this.arr[Math.floor((this.arr.length - 1) / 2)];
         return this.result;
     }
 
 
     lower() {
-        this.arr = this.arr.slice(this.result);
+        this.arr = this.arr.slice(0, this.index);
     }
 
     greater() {
-        this.arr = this.arr.slice(0, this.result - 1);
+        // this.arr = this.arr.slice(this.index - 1);
+        this.arr = this.arr.slice(this.index);
     }
 }
 
